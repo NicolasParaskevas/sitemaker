209b6b9fdcfe8bd4876a52e36ff74b9ac72b13b0
NicolasParaskevas <nicolasparaskevas@hotmail.com>
2023-08-19 20:00:35 +0300

    comments

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	// Usage sitemaker [command] [argument]
+	// Commands:
+	// -> gen [source dir] [output dir]
+	// 	Generates site on the output directory basted on the source dir
+	// -> new [source dir]
+	// 	Creates new project structure
+	// -> help
+	//   Prints all available commands
+	//
+	// source dir must contain:
+	// layouts:
+	// 		- homelayout.html
+	//		- detailslayout.html
+	// components:
+	//		- image.html
+	//		- project-item.html
+	//		- contact-form.html
+	//
+	// data:
+	//  	- homepage.yml
+	//  	- projcets.yml
+	//  	- about.yml
+
 	fmt.Println("Hello World!")
 }
