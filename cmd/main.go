9853d75e644744b9e825526fe4dbcbd252bb9ea0
NicolasParaskevas <nicolasparaskevas@hotmail.com>
2023-08-22 00:31:41 +0300

    check os.Args

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,10 @@ import (
 
 func main() {
 
+	if len(os.Args) < 2 {
+		log.Fatal("please provide a command")
+	}
+
 	command := os.Args[1] // first argument is the command
 	args := os.Args[2:]   // the rest are the command arguments
 
