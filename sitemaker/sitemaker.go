e60b53fc299df5beb5d52a9cfb3f2e65cb805ebf
Nicolas Paraskevas <nicolasparaskevas@hotmail.com>
2024-09-28 16:58:29 +0300

    generate content correctly

--- sitemaker/sitemaker.go
+++ sitemaker/sitemaker.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -12,14 +13,9 @@ import (
 )
 
 type Page struct {
-	Layout  string        `xml:"layout"`
-	View    string        `xml:"view"`
-	Content []ContentItem `xml:"content"`
-}
-
-type ContentItem struct {
-	XMLName xml.Name
-	Value   string `xml:",chardata"`
+	Layout  string `xml:"layout"`
+	View    string `xml:"view"`
+	Content map[string]interface{}
 }
 
 func RunCommand(cmd string, args []string) error {
@@ -85,28 +81,83 @@ func createNewProject(projectDir string) (err error) {
 }
 
 func generateProject(sourceDir, outputDir string) error {
-
 	data, err := loadSourceFiles(sourceDir)
-
 	if err != nil {
 		return err
 	}
 
 	assets, err := loadAssetFiles(sourceDir)
-
 	if err != nil {
 		return err
 	}
 
 	for k, v := range data {
 		var page Page
+
+		// Unmarshal the layout and view sections (static)
 		err := xml.Unmarshal([]byte(v), &page)
 		if err != nil {
 			log.Printf("Error unmarshaling XML: %v", err)
 			continue
 		}
 
-		// add source directory in the front
+		// Now handle <content> part and unmarshal it into a map
+		page.Content = make(map[string]interface{})
+		decoder := xml.NewDecoder(strings.NewReader(v))
+		var currentElement string
+
+		for {
+			// Read tokens from the XML
+			token, err := decoder.Token()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Printf("Error decoding XML: %v", err)
+				return err
+			}
+
+			switch elem := token.(type) {
+			case xml.StartElement:
+				// Check if it's a <content> element
+				if elem.Name.Local == "content" {
+					// Read into a map dynamically
+					for {
+						innerToken, _ := decoder.Token()
+						if innerToken == nil {
+							break
+						}
+
+						switch innerElem := innerToken.(type) {
+						case xml.StartElement:
+							currentElement = innerElem.Name.Local
+						case xml.CharData:
+							contentValue := strings.TrimSpace(string(innerElem))
+
+							if contentValue == "" {
+								continue
+							}
+
+							if existingValue, ok := page.Content[currentElement]; ok {
+								switch v := existingValue.(type) {
+								case []string:
+									// Append to the existing slice
+									page.Content[currentElement] = append(v, contentValue)
+								case string:
+									// Convert single string to a slice of strings
+									page.Content[currentElement] = []string{v, contentValue}
+								}
+							} else {
+								// Set as a single string
+								page.Content[currentElement] = contentValue
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// Add source directory in the front
 		page.Layout = filepath.Join(sourceDir, page.Layout)
 		page.View = filepath.Join(sourceDir, page.View)
 
@@ -119,27 +170,24 @@ func generateProject(sourceDir, outputDir string) error {
 
 		fname := filepath.Base(k)
 		if fname == "." {
-			log.Printf("Error parsing filepath")
+			log.Println("Error parsing filepath")
 			continue
 		}
 
-		// convert .xml to .html
+		// Convert .xml to .html
 		fname = strings.TrimSuffix(fname, filepath.Ext(fname)) + ".html"
 
 		// Add output directory
 		outputFilePath := filepath.Join(outputDir, fname)
 
 		outputFile, err := os.Create(outputFilePath)
-
 		if err != nil {
 			log.Printf("Error creating output file: %v", err)
 			continue
 		}
-
 		defer outputFile.Close()
 
-		err = tmpl.ExecuteTemplate(outputFile, filepath.Base(page.Layout), page)
-
+		err = tmpl.ExecuteTemplate(outputFile, filepath.Base(page.Layout), page.Content)
 		if err != nil {
 			log.Printf("Error executing template: %v", err)
 			continue
